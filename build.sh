81191363e1b4ce9dc100a33e8a6772de7f8a10d1
Muthu Kumar <muthukumar@thefeathers.in>
2018-04-27 21:22:12 +0530

    [build] More sensible temp files

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Babelifying..."
-npx babel ./isEq.js --presets=env -o ./umd/isEq2.js
+npx babel ./isEq.js --presets=env -o ./umd/isEq.temp.js
 echo "Browserifying..."
-npx browserify ./umd/isEq2.js -s isEq > ./umd/isEq.js
+npx browserify ./umd/isEq.temp.js -s isEq > ./umd/isEq.js
 echo "Minifying output..."
 npx uglifyjs ./umd/isEq.js > ./umd/isEq.min.js
 echo "Cleaning up..."
-rm ./umd/isEq2.js
+rm ./umd/isEq.temp.js
 echo "Done!"
